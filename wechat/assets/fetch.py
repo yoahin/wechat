12a466619430d874d439223ffae7c3660722bee6
Yo-ah-in <errelinaaron@gmail.com>
2021-05-23 20:14:35 +0800

    mod: rename root dir; add comments

--- wechat/assets/fetch.py
+++ wechat/assets/fetch.py
@@ -10,7 +10,7 @@ import argparse
 
 ROOT_DIR = os.path.join(os.environ['USERPROFILE'], \
                         'projects', \
-                        'wechat', \
+                        'posts', \
                         'wechat')
 
 parser = argparse.ArgumentParser(description='Fetch the content of an online article')
@@ -25,18 +25,19 @@ args = parser.parse_args()
 
 # NOTE: must use long form as the attr name
 article_url = args.url
+
+# extract article title from the url
 article_title = article_url[article_url.rfind('/')+1:]
 print('Article title is ', article_title)
 
-# write to the file named article_title.html
+# write to the file named article_title.html under the dest passed
 target_folder = args.directory
 if target_folder != None:
     target_file = os.path.join(ROOT_DIR, target_folder, \
                                 article_title + '.html')
-# defaults to None
+# defaults to None; save to current directory then
 else:
     target_file = os.path.join(os.getcwd(), article_title + '.html')
 
 with open(target_file, 'w', encoding='utf-8') as f:
         f.write('this is a simple test')
-
