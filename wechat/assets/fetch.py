b47c14716c88c818e2113381e4db6cfbcc3edeb3
Yo-ah-in <errelinaaron@gmail.com>
2021-05-23 19:50:24 +0800

    add: start refining the scraper

---
+++ wechat/assets/fetch.py
@@ -0,0 +1,42 @@
+# -*- coding=utf-8 -*-
+
+# This script will parse URL and path passed on command line
+# and output the scraped content to a html file stored in the path
+# Author： Errelin
+
+import os.path
+import os
+import argparse
+
+ROOT_DIR = os.path.join(os.environ['USERPROFILE'], \
+                        'projects', \
+                        'wechat', \
+                        'wechat')
+
+parser = argparse.ArgumentParser(description='Fetch the content of an online article')
+
+# command line args
+parser.add_argument('-u', '--url', help='the url of which to be scraped')
+parser.add_argument('-d', '--directory', \
+                    help='destination where the output will be stored')
+
+
+args = parser.parse_args()
+
+# NOTE: must use long form as the attr name
+article_url = args.url
+article_title = article_url[article_url.rfind('/')+1:]
+print('Article title is ', article_title)
+
+# write to the file named article_title.html
+target_folder = args.directory
+if target_folder != None:
+    target_file = os.path.join(ROOT_DIR, target_folder, \
+                                article_title + '.html')
+# defaults to None
+else:
+    target_file = os.path.join(os.getcwd(), article_title + '.html')
+
+with open(target_file, 'w', encoding='utf-8') as f:
+        f.write('this is a simple test')
+
