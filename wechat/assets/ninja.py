ca79307ab67c60d00595130f252f0d8c01fb5ef0
Linerre <leon.errelin@outlook.com>
2021-06-16 23:55:37 +0800

    refine arg metavar and help info

--- wechat/assets/ninja.py
+++ wechat/assets/ninja.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-This script will use base templates under ./templates and 
+This script will use base templates under ./templates and
 children templates in various dirs under ../wechat/*/ to
 create the html files used for wechat posts.
 """
@@ -10,40 +10,40 @@ create the html files used for wechat posts.
 from jinja2 import Environment, FileSystemLoader
 import argparse
 from os.path import abspath, basename, join
-from os import environ, name
 
 #TODO
 # [X] pass the article dir as a tempalte folder
 # [X] test if include will work
-if name == 'nt':
-    ROOT_DIR = join(environ['USERPROFILE'], 'projects', 'posts', 'wechat')
-elif name == 'posix':
-    ROOT_DIR = join(environ['HOME'], 'projects', 'posts', 'wechat')
 
 parser = argparse.ArgumentParser(description='Create template htmls for posts')
 
 # read the doc: https://docs.python.org/dev/library/argparse.html#dest
 # 1st arg: article dir where children templates stored
 parser.add_argument('-d', '--article-directory',
-                    metavar='basename of the article directory',
-                    default='.',
-                    help='basename of article directory where children \
-                          templates are stored (e.g. peace); defaults to ./')
+                    metavar='ARTICLE_DIR_BASENAME',
+                    default=basename('.'),
+                    help='Basename of the article directory where children \
+                          templates are stored (e.g. peace); defaults to\
+                          $(basename "."). See --post-part option for more\
+                          information on how args will be used to set the \
+                          needed paths.')
 # 2nd arg: article part to be posted
 # NOTE: git-tracking current.html so that each time on different machine
 # its content along with header, dicts or so will be populated to num.html
 parser.add_argument('-p', '--post-part',
                     nargs=2,
-                    metavar=('news source (e.g. Economist)', 'part number'),
-                    default=('.', '1'),
+                    metavar=('NEWS_SOURCE', 'PART_NUMBER'),
+                    default=(basename('.'), '1'),
                     help='The article part to be posted;\
-                         NOTE: this option is used to create output file for\
-                         posting. The output file will be named based on num\
-                         given, for example: 2.html (i.e. part 2 of an \
-                         article). Defaults to the current directory.')
+                         NOTE: this option is used to create output file under\
+                         ROOT_DIR/NEWS_SOURCE/ARTICLE_DIR_BASENAME. ROOT_DIR \
+                         will always be automatically set to the following:\
+                         $HOME/projects/posts/wechat.  The output file will \
+                         be named based on num given, for example: 2.html \
+                         (i.e.part 2 of an article). NEWS_SOURCE defaults to\
+                         the current directory, PART_NUMBER to 1.')
 # 3rd arg: base template to be used
 parser.add_argument('-b', '--base-template',
-                    metavar='base template file name',
                     default='new-yorker-base.html',
                     help='The base template; (default: new-yorker-base.html)')
 
@@ -51,8 +51,8 @@ parser.add_argument('-b', '--base-template',
 args = parser.parse_args()
 
 # each time article part template will be updated: 1.html, 2.html, 3.html, ...
-article_source = join(ROOT_DIR, args.post_part[0])
-article_dir = join(ROOT_DIR, article_source, args.article_directory)
+article_source = abspath(join('.', args.post_part[0]))
+article_dir = abspath(join('.', article_source, args.article_directory))
 part_num = args.post_part[1]
 base_template = args.base_template
 
@@ -82,3 +82,4 @@ output = template.render(
         title=f'{args.article_directory}-{part_num}')
 with open(join(article_dir, part_num+'.html'), 'w', encoding='utf-8') as f:
     f.write(output)
+
