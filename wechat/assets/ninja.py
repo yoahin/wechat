f01b9ac9bbe6bb513c5b0260ad6779ce89997c78
Yo-ah-in <errelinarron@gmail.com>
2021-06-21 21:20:41 +0800

    improve the defaults

--- wechat/assets/ninja.py
+++ wechat/assets/ninja.py
@@ -37,22 +37,22 @@ parser.add_argument('-s', '--news-source',
 parser.add_argument('-p', '--post-part',
                     nargs=2,
                     metavar=('TITLE_KEYWORD', 'PART_NUM'),
-                    default=('assets', '1'),
+                    default=('nil', '1'),
                     help='The article part to be posted;\
                          NOTE: this option is used to create output file under\
-                         ROOT_DIR/NEWS_SRC/TITLE_KEYWORD. ROOT_DIR \
+                         ROOT_DIR/NEWS_SRC/TITLE_KEYWORD. ROOT_DIR\
                          will always be automatically set to the following:\
-                         $HOME/projects/posts/wechat. The output file will \
+                         $HOME/projects/posts/wechat. The output file will\
                          be named based on PART_NUM given, for example: 2.html\
-                          (i.e.part 2 of the article). TITLE_KEYWORD defaults \
-                         to "assets"; PART_NUM defaults to 1. Example:\
-                         passing "cyber 1" will make TITLE_KEYWORD cyber and\
-                         PART_NUM 1.')
+                          (i.e.part 2 of the article). TITLE_KEYWORD defaults\
+                         to "nil"; PART_NUM defaults to 1. Example:\
+                         passing "cyber 1" will make TITLE_KEYWORD "cyber" and\
+                         PART_NUM "1".')
 # 3rd arg: base template to be used
 parser.add_argument('-b', '--base-template',
-                    default='new-yorker-base.html',
+                    default='example-base.html',
                     help='The base template file name, with extension.\
-                          Default: new-yorker-base.html')
+                          Default: example-base.html')
 
 # parse all args
 args = parser.parse_args()
@@ -61,19 +61,17 @@ ROOT_DIR = join(expanduser('~'), 'projects', 'posts', 'wechat')
 
 # each time article part template will be updated: 1.html, 2.html, 3.html, ...
 news_source = args.news_source
-assets_dir = join(ROOT_DIR, 'assets')
 article_dir = join(ROOT_DIR, news_source, args.post_part[0])
 if not exists(article_dir):
     makedirs(article_dir)
 part_num = args.post_part[1]
 
 file_loader = FileSystemLoader(
-        ['templates',
+        [
          'templates/economist',
          'templates/dicts',
          'templates/twitter',
          'templates/icons',
-         assets_dir,
          article_dir
          ])
 env = Environment(loader=file_loader)
@@ -82,12 +80,10 @@ print(env.list_templates(extensions=["html"]))
 # NOTE
 # FileSystemLoader will also load templates in subdirectories
 # but it recognizes those templates as they are: /path/to/template
-# if parent='string' given, then it would be string/path/to/template
-# which in turn leads to TemplateNotFound error
 
-# NOTE: template file should be a variable
-# template should be the one you want to get from output
-# i.e. the one that extends the base
+# NOTE:
+# The final result should be the template that extends/import other
+# templates, rather than the base templates themselves.
 
 template = env.get_template('current.html')
 
