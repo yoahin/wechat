17305a85374b731b0bcacfa084d3c04527c5be16
Yo-ah-in <errelinaaron@gmail.com>
2021-07-17 16:15:58 +0800

    add sense number option to target more precise

--- wechat/assets/mwth-fetcher.py
+++ wechat/assets/mwth-fetcher.py
@@ -8,23 +8,27 @@ The syns will be stored in a list
 
 import argparse
 import requests
-from os.path import abspath, exists, join
 from lxml import html
+from os.path import abspath, exists, join
+
 
 parser = argparse.ArgumentParser(description='Fetch synonyms of a ginve word from Merriam-Webster')
 
-# 1st arg: Merriam-Webster word page url
-parser.add_argument('-u', '--url', 
-										help='URL of a word page')
+# 1st arg: Sense num of a given word
 
-# 2nd arg: 
-parser.add_argument('-o', '--out-put',
-										help='Output to a file instead of stdout')
+parser.add_argument('-s', '--sense-num', help="Sense number of the word (Default: none)")
+
+# 2nd arg: Merriam-Webster word page url
+parser.add_argument('-u', '--url', help='URL of the word\' dictionary page')
+
+# 3rd arg: 
+parser.add_argument('-o', '--out-put', help='Output to a file instead of stdout')
 
 args = parser.parse_args()
 
 
 # get the values of args
+sensenum = args.sense_num
 url = args.url
 output = args.out_put
 
@@ -32,9 +36,12 @@ raw_content = requests.get(url).text
 etree = html.fromstring(raw_content)
 
 # TODO: fix bug -- group the fetched words according the original page
-syns = etree.xpath('//span[contains(@class, "re-list")]\
-	/div[contains(@class, "synonyms_list")]\
-	/ul[contains(@class, "mw-list")]/li/a/text()')
+if sensenum:
+	# syn_blocks consist of 1) def & e.g.s, 2) synonyms, 3) related words
+	syn_blocks = etree.xpath(f'//span[contains(@class, "sense-{sensenum}")]/following-sibling::*')
+else:
+	raise SystemExit('No sense number given; Execution terminated')
+
 
 if not output:
-	print(syns)
+	print(syn_blocks)
