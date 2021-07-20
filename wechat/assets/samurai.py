8704d7ab0bab6e7d34fab280bce51baa04dd9e0c
Yo-ah-in <errelinaaron@gmail.com>
2021-07-20 23:34:42 +0800

    start building dict template generator

---
+++ wechat/assets/samurai.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+This scirpt depends on mw-fetch.py and mwth-fetch.py;
+It will use the fetched content to generate
+the needed word templates
+'''
+
+# standard or 3rd party libs
+import argparse
+from os.path import join
+
+import ./fetchers/mwth-fetch.py
+
+ah
+akjkhkh
+
+
