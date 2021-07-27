c3d18cf426ddd348ba83befbf0f66e43591312fa
Yo-ah-in <errelinaaron@gmail.com>
2021-07-27 15:52:10 +0800

    modules ready

--- wechat/assets/samurai.py
+++ wechat/assets/samurai.py
@@ -6,12 +6,31 @@ the needed word templates
 '''
 
 # standard or 3rd party libs
-import argparse
+import requests
+from lxml import html
 from os.path import join
 
-import ./fetchers/mwth-fetch.py
+# my own module
+from fetchers.mwth import get_target, get_blocks, get_synonyms
 
-ah
-akjkhkh
+if __name__ == '__main__':
+    sensenum, url, raw_html, output = get_target()
+    # Detect raw html based on args passed
+    if url:
+        raw_content = requests.get(url).text
 
+    if raw_html:
+        raw_content = raw_html
 
+    etree = html.fromstring(raw_content)
+    syn_blocks = get_blocks(etree, sensenum)
+
+    # thes list keywords: 'syn', 'rel', 'phrase', 'near', 'ant'
+    if not output:
+        print(syn_blocks)
+
+        hed, lst, usg, vrt = get_synonyms(syn_blocks, 'rel')
+        print(hed, lst, usg, vrt, sep='\n')
+        # print(get_sense())
+    if output:
+        pass
