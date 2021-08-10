7fb7322e94e808c61cdbaa5db8518ef7448a966c
Yo-ah-in <errelinaaron@gmail.com>
2021-08-10 23:58:18 +0800

    add xpaths module

---
+++ wechat/assets/xpaths.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+'''
+This script holds various types of xpath used for
+scrapping content from different online dictionaries.
+It may be further splitted into smaller modules in future
+should the need arises.
+'''
