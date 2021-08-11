a9ba5b5d5c9537842c81b275fd8d58f6f1325e4e
Yo-ah-in <errelinaaron@gmail.com>
2021-08-11 20:22:09 +0800

    fix unsafe variable error

--- wechat/assets/xpaths.py
+++ wechat/assets/xpaths.py
@@ -1,7 +1,13 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 '''
 This script holds various types of xpath used for
 scrapping content from different online dictionaries.
 It may be further splitted into smaller modules in future
 should the need arises.
 '''
+
+WMTH_XPATHS = {
+    'entry': ''
+}
+
+WM_XPATHS = {}
